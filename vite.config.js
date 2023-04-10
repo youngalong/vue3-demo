3c7fd1825f11acaf737e88c35f50a42833c194a3
youngalong <youngalone@163.com>
2023-04-10 19:58:43 +0800

    完成增删改查功能

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    /* 是否开启 $ref , vue3.2 的语法糖 */
+    vue({
+      refTransform: true,
+      reactivityTransform: true
+    }),
+  ],
+  base: './', //打包相对路径
 })
